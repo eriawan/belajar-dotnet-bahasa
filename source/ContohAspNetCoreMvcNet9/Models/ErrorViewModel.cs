34033e2861b6b6dcb256a6ff74be90b8adbb3328
eriawan <eriawan@live.com>
2025-01-11 03:44:51 +0700

    initial addition of source folder with sample files

---
+++ source/ContohAspNetCoreMvcNet9/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ContohAspNetCoreMvcNet9.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ source/ContohWinformsNet9/Form1.cs
@@ -0,0 +1,10 @@
+namespace ContohWinformsNet9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
