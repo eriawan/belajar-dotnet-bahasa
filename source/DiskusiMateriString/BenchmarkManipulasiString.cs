94ab77ae9960cf80a4448897ee19aa9299ede913
eriawan <eriawan@live.com>
2025-03-14 01:50:03 +0700

    Push commit tentang materi pembuka String

---
+++ source/DiskusiMateriString/BenchmarkManipulasiString.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskusiMateriString
+{
+    [MemoryDiagnoser]
+    public class BenchmarkManipulasiString
+    {
+        [Params(100, 1_000, 4_000)]
+        public int JumlahLoop;
+
+        [Benchmark]
+        public void SubstringBiasa()
+        {
+            for (int i = 0; i < JumlahLoop; i++)
+            {
+                string anystring = "Halo Eriawan".Substring(5); 
+            }
+        }
+
+        [Benchmark]
+        public void SubstringPakaiSpan()
+        {
+            for (int i = 0; i < JumlahLoop; i++)
+            {
+                string anystring = new string("Halo Eriawan".AsSpan(5)); 
+            }
+        }
+    }
+}
---
+++ source/DiskusiMateriString/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DiskusiMateriString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo, teman-teman!");
+            //string string01 = "string1";
+            //string string02 = "string2";
+            //// string concat sederhana
+            //string string03 = string01 + string02;
+            //// string concat dengan string interpolation
+            //string string04 = $"{string01}{string02}";
+            //// string concat antara string dengan non string (contoh: Int16)
+            //short contohInt16 = (short)-12345;
+            //// string concat antara string dengan non string (contoh: Int16) 
+            //// memakai string interpolation
+            //string string05 = $"{string04}{contohInt16}";
+            //// string concat antara string dengan non string (contoh: Int16) 
+            //// tidak memakai string interpolation
+            //string string06 = string04 + contohInt16;
+            //// coba cetak hasilnya dari di atas
+            //Console.WriteLine($"string05 ={string05}");
+            //Console.WriteLine($"string06 ={string06}");
+            //// contoh substring
+            //string string07 = "nama: Eriawan Kusumawardhono".Substring(6);
+            //Console.WriteLine($"string07 = {string07}");
+            string anystring = "Eriawan 😊👍ドページを切り替える方法";
+            Console.WriteLine(anystring);
+            //BenchmarkRunner.Run<BenchmarkManipulasiString>();
+        }
+    }
+}
