11118c5d3b3972dcc9137eacca0c806526defdd4
eriawan <eriawan@live.com>
2025-06-19 02:00:13 +0700

    Add the sample code for ValueTask discussion in detail with sample benchmark.

---
+++ source/DiskusiContohSkenarioValueTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DiskusiContohSkenarioValueTask
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Sample ValueTask<T>");
+            //SimpleWebContentReader reader = new SimpleWebContentReader();
+            //// call fisrt time...
+            //string webKompasContent = await reader.GetWebContentUsingTask();
+            ////Console.WriteLine(webKompasContent.Substring(200));
+            //Thread.Sleep(200);
+            //// call again
+            //webKompasContent = await reader.GetWebContentUsingTask();
+            //Console.WriteLine("second run");
+
+            //Benchmark Task and ValueTask
+            BenchmarkRunner.Run<SimpleWebContentReader>();
+        }
+    }
+}
---
+++ source/DiskusiContohSkenarioValueTask/SimpleWebContentReader.cs
@@ -0,0 +1,83 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Runtime.Caching;
+
+namespace DiskusiContohSkenarioValueTask
+{
+    [MemoryDiagnoser]
+    public class SimpleWebContentReader
+    {
+        HttpClient httpClient = new HttpClient();
+        string KompasComContent = string.Empty;
+        MemoryCache memcache = MemoryCache.Default;
+        const string WEBKOMPAS = "webkompas";
+
+        public SimpleWebContentReader()
+        {
+            //memcache[WEBKOMPAS] = string.Empty;
+
+        }
+
+        [Params(50000, 100000, 200000)]
+        public int NumberOfLoop;
+
+        [Benchmark]
+        public async Task GetWebContentBenchmarkTask()
+        {
+            for (int i = 0; i < NumberOfLoop; i++)
+            {
+                object webcontentObj = memcache.Get(WEBKOMPAS);
+                string webcontent = string.Empty;
+                if (webcontentObj is null)
+                {
+                    CacheItemPolicy cachewebKompas = new CacheItemPolicy();
+                    cachewebKompas.SlidingExpiration = TimeSpan.FromHours(1);
+                    webcontent = await httpClient.GetStringAsync("https://www.kompas.com");
+                    memcache.Set(WEBKOMPAS, webcontent, cachewebKompas);
+                }
+                else
+                {
+                    webcontent = memcache.Get(key: WEBKOMPAS).ToString()!;
+                } 
+            }
+        }
+
+        [Benchmark]
+        public async ValueTask GetWebContentBenchmarkValueTask()
+        {
+            for (int i = 0; i < NumberOfLoop; i++)
+            {
+                object webcontentObj = memcache.Get(WEBKOMPAS);
+                string webcontent = string.Empty;
+                if (webcontentObj is null)
+                {
+                    CacheItemPolicy cachewebKompas = new CacheItemPolicy();
+                    cachewebKompas.SlidingExpiration = TimeSpan.FromHours(1);
+                    webcontent = await httpClient.GetStringAsync("https://www.kompas.com");
+                    memcache.Set(WEBKOMPAS, webcontent, cachewebKompas);
+                }
+                else
+                {
+                    webcontent = memcache.Get(key: WEBKOMPAS).ToString()!;
+                } 
+            }
+        }
+
+        internal async Task<string> GetWebContentUsingTask()
+        {
+            object webcontentObj = memcache.Get(WEBKOMPAS);
+            string webcontent = string.Empty;
+            if (webcontentObj is null)
+            {
+                CacheItemPolicy cachewebKompas = new CacheItemPolicy();
+                cachewebKompas.SlidingExpiration = TimeSpan.FromSeconds(200);
+                webcontent = await httpClient.GetStringAsync("https://www.kompas.com");
+                memcache.Set(WEBKOMPAS, webcontent, cachewebKompas);
+            } 
+            else
+            {
+                webcontent = memcache.Get(key: WEBKOMPAS).ToString()!;
+            }
+            return webcontent;
+        }
+    }
+}
